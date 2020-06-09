785df6814ca8c094b4c0d5051d77ea27e794f299
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-06-08 22:13:50 -0700

    added the regex function and user input to search the api results

--- main.py
+++ main.py
@@ -19,41 +19,48 @@ def welcome_information():
 
   Press (q) to quit at any time
   '''))
+  user_input()
 
-def user_input():
+def reset_user_fav_list():
+  user_fav_list = []
+  return user_fav_list
+
+def user_input(user_fav_list = []):
   order_now = input(dedent(
   '''
   Would you like to order anything from our store (y/n)?
   '''))
   if order_now == 'y':
-    search_product()
+    search_product(user_fav_list)
   elif order_now=='n' or order_now=='q':
     print("Thank you! Please come again. ")
   else:
     print("Please re-enter with y or n")
     user_input()
 
-def search_product():
+def search_product(user_fav_list):
   search_product = input(dedent(
     '''
     What would you like to order?
     '''
   ))
   search_product = search_product.lower()
-  find_search_product(search_product)
+  find_search_product(search_product,user_fav_list)
 
 
-def find_search_product(search_product):
-    regex_dict = {'mascara':'\b(\w*.ascara\w*)\b', 'foundation': '\b(\w*.oundation\w*)\b', 'eye shadow': '\b(\w*.hadow\w*)\b', 'lip products': '\b(\w*.ip\w*)\b', 'bronzer': '\b(\w*.onzer\w*)\b', 'liner': '\b(\w*.iner)\b', 'pencil' : '\b(\w*.encil)\b', 'blush' : '\b(\w*.lush)\b', 'cream' : '\b(\w*.ream\w*)\b', 'moisturizer': '\b(\w*.oistu\w*)\b', 'nail': '\b(\w*.ail\w*)\b', 'primer': '\b(\w*.rimer\w*)\b', 'powder': '\b(\w*.owder\w*)\b'}
+def find_search_product(search_product, user_fav_list):
+    regex_dict = {'mascara':'\w*.ascara\w*', 'foundation': '\w*.oundation\w*', 'eye shadow': '\w*.hadow\w*', 'lip products': '\w*.ip\w*', 'bronzer': '\w*.onzer\w*', 'liner': '\w*[Ll]iner\w*', 'pencil' : '\w*.encil', 'blush' : '\w*.lush', 'cream' : '\w*.ream\w*', 'moisturizer': '\w*.oistu\w*', 'nail': '\w*.ail\w*', 'primer': '\w*.rimer\w*', 'powder': '\w*.owder\w*'}
     pattern = str(regex_dict[search_product])
-    
     vegan_makeup_list = beauty_api_call()
     for item in vegan_makeup_list:
-      print(item['name'])
-# #     #check item['name'] to search_product
-#         products = re.search(pattern,item['name'])
-#         print(products)
+      if re.search(pattern,item['name'].strip()):
+        user_fav_list.append(item)
+    # print(len(user_fav_list))
+    user_input(user_fav_list)
+ 
+  
 
+    
 
 
 
@@ -65,4 +72,3 @@ def find_search_product(search_product):
 
 if __name__ == "__main__":
     welcome_information()
-    user_input()
