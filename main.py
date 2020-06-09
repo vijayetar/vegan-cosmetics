a64e5299249dddba9bcc39b0131d6930cd3974ea
corey-marchand <corey.marchand@me.com>
2020-06-09 12:06:29 -0700

    debugging

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from vegan_cosmetics.search_with_api.beauty_api import beauty_api_call
 #save to text file
 
 def welcome_information():
+  """
+  Initial function.  Greets the user and then calls the user_input function which ultimately allows the user to say whether or not they want to order from the store.
+  """
   print(dedent('''
   *****************************************
 
@@ -21,10 +24,16 @@ def welcome_information():
   user_input()
 
 def reset_user_fav_list():
+  """
+  Function that resets the users favorite list
+  """
   user_fav_list = []
   return user_fav_list
 
 def user_input(user_fav_list = []):
+  """
+  Function takes in user input whether or not they want to order anything from our store, if they do, then it brings in the search products function and allows them to order.  If they dont, the program will quit. Also Covers the edge case, if they enter the wrong input, it will ask them the question again.  
+  """
   order_now = input(dedent(
   '''
   Would you like to order anything from our store (y/n)?
@@ -38,49 +47,48 @@ def user_input(user_fav_list = []):
     user_input()
 
 def search_product(user_fav_list):
+  """
+  Ask the user what they would like to order, then it plugs the input into the find_search_product helper function which searchs the products in the databse based on regex.
+  """
   search_product = input(dedent(
     '''
     What would you like to order?
     '''
   ))
+  
   search_product = search_product.lower()
   find_search_product(search_product,user_fav_list)
 
 
 def find_search_product(search_product, user_fav_list):
+    """
+    function has a dictionary of regex terms, then it iterates through a list of data and if the regex matches up with the search, it appends the items to user_fav_list.  
+    """
     regex_dict = {'mascara':'\w*.ascara\w*', 'foundation': '\w*.oundation\w*', 'eye shadow': '\w*.hadow\w*', 'lip products': '\w*.ip\w*', 'bronzer': '\w*.onzer\w*', 'liner': '\w*[Ll]iner\w*', 'pencil' : '\w*.encil', 'blush' : '\w*.lush', 'cream' : '\w*.ream\w*', 'moisturizer': '\w*.oistu\w*', 'nail': '\w*.ail\w*', 'primer': '\w*.rimer\w*', 'powder': '\w*.owder\w*'}
     pattern = str(regex_dict[search_product])
     vegan_makeup_list = beauty_api_call()
     for item in vegan_makeup_list:
       if re.search(pattern,item['name'].strip()):
-        user_fav_list.append(item)
+        user_fav_list.append(item['name'])
     # user_fav_list=list(set(user_fav_list))
-
-    print(user_fav_list)
+    # print(user_fav_list)
     user_input(user_fav_list)
 
-def save_user_product(user_saved_input):
-  order_save = input(dedent(
-  '''
-  Would you like to save your products?
-  '''))
-  if search_product > 0:
-
-    with open("./assets/vegan_cosmetics_saved.txt", "w") as file:
-      for saved in user_input: 
-        for key, value in item.items():
-          string = f"{key}: {value} \n\n"
-          file.write("".join(string))
-
-    
-save_user_product('mascarra')
-
-
-
-
-
-
-
+# def save_user_product(user_saved_input, user_fav_list):
+#   """
+#   A function that will give the user and option to save their searched products.  If the user determines to save the products, it will write the products to a new database, if not, it will discard the products that they have searched.  
+#   """
+#   if user_input(user_fav_list) > 0:
+#     order_save = input(dedent(
+#     '''
+#     Would you like to save your products?
+#     '''))
+#     if order_save == 'y':
+#       with open("./assets/vegan_cosmetics_saved.txt", "w") as file:
+#         for saved in user_input: 
+#           for key, value in item.items():
+#             string = f"{key}: {value} \n\n"
+#             file.write("".join(string))
 
 if __name__ == "__main__":
-    welcome_information()
+    welcome_information() 
