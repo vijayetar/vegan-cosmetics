e09664ec8098cfa37617a77061a7a0b29234c178
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-06-09 13:59:03 -0700

    scraped 100percent pure completely

--- vegan_cosmetics/web_scraper/testing_scraper.py
+++ vegan_cosmetics/web_scraper/testing_scraper.py
@@ -5,26 +5,30 @@ test_url = "https://www.100percentpure.com/collections/fruit-dyed-makeup"
 
 def get_contents_100percentpure():
   print('This is the website:  ', '100percent pure')
-  # response = requests.get(test_url)
-  # content = response.content
-  # soup = BeautifulSoup(content, 'html.parser')
-  # # print(soup.prettify())
-  # cost = soup.findall('span', class_= 'original_price')
-  # # <span class="original_price"><span class="money notranslate" data-gip-price-id="fyoFpoBTUfPfGOov" data-gip-original-value="22" data-gip-converted="false">$22.00</span></span>
-  # # results = soup.find('div', id = "tab-description")
-  # name = soup.findall('span', class_ = 'text-center m-t-sm flex j-c-center caption_text')
-  # # <span class="text-center m-t-sm flex j-c-center caption_text">
-  # #   <a href="/collections/fruit-dyed-makeup/products/fruit-pigmented-lip-gloss"><span class="same-height inline" same-height-id="productName" style="height: 16px;">Fruit Pigmented® Lip Gloss</span></a>
-  # # </span>
-  # print("*" * 100)
-  # # print(results.get_text().strip())
-  # # cost returns too many prices, isolate the one we want with an index value
-  # for price in cost:
-  #   print(price.get_text())
-  # for item in name:
-  #   print(item.get_text())
-  # print("*" * 50)
+  response = requests.get(test_url)
+  content = response.content
+  soup = BeautifulSoup(content, 'html.parser')
+  # print(soup.prettify())
+  both = soup.find_all('div', class_= 'product-bottom')
+  print('This is the website:  ', '100 Percent Pure')
+  for item in both:
+    deeper_url = f"https://www.100percentpure.com{item.span.a['href']}"
+    get_contents(deeper_url)
+    print("\n")
 
+def get_contents(deeper_url):
+  response = requests.get(deeper_url)
+  content = response.content
+  soup = BeautifulSoup(content, 'html.parser')
+  # print(soup.prettify())
+  cost = soup.find('span', class_ = 'section_title_text inline original_price' )
+  results = soup.find('div', class_ = 'm-b-sm text-base l-h ingredient-container')
+  name = soup.find('h1', class_ = 'section_title_text')
+  print("*" * 100)
+  print(f"Name : {name.get_text().strip()}")
+  print(f"Cost : {cost.get_text().strip()}")
+  print(f"Ingredients:  {results.get_text().strip()}")
+  print("*" * 100)
 
 if __name__ == "__main__":
   get_contents_100percentpure()
